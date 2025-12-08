c3281e7e60699f259d2b31ec9f40d83fdb399bd8
Mahad Kalam <mahadkalam1234@gmail.com>
2025-12-08 16:16:15 +0000

    Code cleanup + fix regions

--- src/scraper.rs
+++ src/scraper.rs
@@ -90,57 +90,34 @@ fn scrape_region(region: &Region, csrf_token: &String) -> Result<ShopItems> {
     let selector = Selector::parse(".shop-item-card").unwrap();
     let mut items = Vec::new();
     for element in document.select(&selector) {
-        let selector = Selector::parse("h4").unwrap();
-        let title = element
-            .select(&selector)
-            .next()
-            .ok_or_else(|| eyre!("missing title element"))?
-            .inner_html();
-
-        let selector = Selector::parse("p.shop-item-card__description").unwrap();
-        let description = element
-            .select(&selector)
-            .next()
-            .ok_or_else(|| eyre!("missing description element"))?
-            .inner_html();
-
-        let selector = Selector::parse("span.shop-item-card__price").unwrap();
-        let price_text = element
-            .select(&selector)
-            .next()
-            .ok_or_else(|| eyre!("missing price element"))?
+        let title = select_one(&element, "h4")?.inner_html();
+        let description = select_one(&element, "p.shop-item-card__description")?.inner_html();
+        let price: u32 = select_one(&element, "span.shop-item-card__price")?
             .text()
-            .next()
-            .unwrap();
-        let price: u32 = price_text
+            .collect::<String>()
             .chars()
             .filter(|c| c.is_ascii_digit())
             .collect::<String>()
-            .parse()
-            .map_err(|_| eyre!("missing title element"))?;
-
-        let selector = Selector::parse("div.shop-item-card__image > img").unwrap();
-        let image_url = element
-            .select(&selector)
-            .next()
-            .ok_or_else(|| eyre!("missing image element"))?
+            .parse()?;
+        let image_url: Url = select_one(&element, "div.shop-item-card__image > img")?
             .attr("src")
-            .ok_or_else(|| eyre!("found image element but can't find its source"))?;
-        let image_url = Url::parse(image_url).unwrap();
+            .ok_or_else(|| eyre!("missing image src"))?
+            .parse()?;
 
-        let selector = Selector::parse("div.shop-item-card__order-button > a.btn").unwrap();
-        let href: Url = element
-            .select(&selector)
-            .next()
-            .ok_or_else(|| eyre!("Failed to find shop order button"))?
+        let href: Url = select_one(&element, "div.shop-item-card__order-button > a.btn")?
             .attr("href")
             .ok_or_else(|| eyre!("missing shop order button's url"))?
-            .parse()
-            .unwrap();
+            .parse()?;
+
         let shop_item_id: ShopItemId = href
             .query_pairs()
-            .find(|(k, _)| k == "shop_item_id")
-            .and_then(|(_, v)| v.parse().ok())
+            .find_map(|(k, v)| {
+                if k == "shop_item_id" {
+                    v.parse().ok()
+                } else {
+                    None
+                }
+            })
             .ok_or_else(|| eyre!("can't find or parse shop item id"))?;
 
         items.push(ShopItem {
@@ -179,19 +156,27 @@ pub fn scrape() -> Result<Vec<ShopItem>> {
     for region in Region::VARIANTS {
         let region_items = scrape_region(region, &csrf_token)?;
 
-        for item in region_items.into_iter() {
-            match items.get_mut(&item.id) {
-                Some(existing) => {
-                    existing.regions.push(region.clone());
-                }
-                None => {
+        for item in region_items {
+            items
+                .entry(item.id)
+                .and_modify(|e| e.regions.push(region.clone()))
+                .or_insert_with(|| {
                     let mut new_item = item;
                     new_item.regions = vec![region.clone()];
-                    items.insert(new_item.id, new_item);
-                }
-            }
+                    new_item
+                });
         }
     }
 
     Ok(items.into_values().collect())
+}
+
+fn select_one<'a>(
+    element: &'a scraper::ElementRef,
+    selector: &str,
+) -> Result<scraper::ElementRef<'a>> {
+    element
+        .select(&Selector::parse(selector).unwrap())
+        .next()
+        .ok_or_else(|| eyre!("missing element: {}", selector))
 }
